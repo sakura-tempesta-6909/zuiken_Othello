592677f6268f95409c58b9b8a654cf15bf291bd9
zuiken-code <zuiken2022robo@gmail.com>
2024-10-27 22:44:08 +0900

    add make board function

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String [] args){
         System.out.println("hello");
+        makeBoard(8);
+    }
+    public static int[][] makeBoard(int size){
+        int [][]boared = new int[size][size];
+        Arrays.fill(boared, 0); //すべてのマスを0で埋める(配列定義時はすべて0だからいらない気もするが一応)
+        return boared;
     }
 }
+
+//列: row
+//行: column
